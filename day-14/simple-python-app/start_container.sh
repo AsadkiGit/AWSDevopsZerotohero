266e8907b1563e7b486e76881bf7f6a76fc78266
AsadkiGit <aabdullah@octavebytes.com>
2024-10-15 16:04:11 +0500

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull aabdullahoctavedocker/simple-python-flask-app
+  
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 aabdullahoctavedocker/simple-python-flask-app
