8329412ba07d5955e7b2f1caf54e9799c192d1bf
AsadkiGit <aabdullah@octavebytes.com>
2024-10-15 14:40:55 +0500

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,6 +3,6 @@ set -e
 
 # Stop the running container (replace <container_name> with your actual container name or ID)
 docker stop $(docker ps -q --filter ancestor=aabdullahoctavedocker/simple-python-flask-app)
-
+echo
 # Remove the stopped container
 docker rm $(docker ps -a -q --filter ancestor=aabdullahoctavedocker/simple-python-flask-app)
